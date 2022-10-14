32f293877a7e0039eda48537a95f83636b523d88
Namejess <jessy_drouin@protonmail.com>
2022-10-14 10:15:57 +0200

    Unit Tests

--- src/test/java/com/openclassrooms/api/EmployeeControllerTest.java
+++ src/test/java/com/openclassrooms/api/EmployeeControllerTest.java
@@ -5,24 +5,37 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class EmployeeControllerTest {
 
+
+//    @Autowired
+//    private WebApplicationContext context;
     @Autowired
     private MockMvc mockMvc;
+
+//    @BeforeEach
+//    public void beforeAll() {
+//        mockMvc = MockMvcBuilders
+//                .webAppContextSetup(context)
+//                .build();
+//    }
 
     @Test
     public void testGetEmployees() throws Exception {
         mockMvc.perform(get("/employees"))
-                .andExpect(status().isOk());
-//                .andExpect(jsonPath("$[0].firstName", is("Laurent")));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].firstName", is("Laurent")));
     }
 
 }
